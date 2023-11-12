04ba69aab048dd6362a919897be03c21586da7be
Emmanuel <obengagyen@gmail.com>
2023-11-12 13:57:23 +0000

    initial commit

---
+++ flaskr/__init__.py
@@ -0,0 +1,33 @@
+"""
+Initialize the app
+"""
+import os
+
+from flask import Flask
+
+
+def create_app(test_config: str = None):
+    """Create and configure the app"""
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="development",
+        DATABASE=os.path.join(app.instance_path, "beige.sqlite3")
+    )
+
+    if test_config:
+        # Load config from file if it exists
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    # ensure os path exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/hello")
+    def hello():
+        return "Hello World!!!"
+    
+    return app
---
+++ flaskr/db.py
@@ -0,0 +1,16 @@
+"""
+Model - Database logic goes here
+"""
+import sqlite3
+
+import click
+from flask import current_app, g
+
+
+def get_db():
+    """Function to connect to the database"""
+    if "db" not in g:
+        g.db = sqlite3.connect(
+            current_app.config["DATABASE"]
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
