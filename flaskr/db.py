f3b46c862404dc278244058d57fd3f7a183fa231
Emmanuel <obengagyen@gmail.com>
2023-11-12 13:59:11 +0000

    ignore database file

--- flaskr/db.py
+++ flaskr/db.py
@@ -11,6 +11,6 @@ def get_db():
     """Function to connect to the database"""
     if "db" not in g:
         g.db = sqlite3.connect(
-            current_app.config["DATABASE"]
+            current_app.config["DATABASE"],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
